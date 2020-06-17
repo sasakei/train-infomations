ac2e0f10fc5204049b953654c86890340c80333a
sasaki kei <sasakei1021@icloud.com>
2020-06-17 16:06:21 +0900

    news-index

--- App.js
+++ App.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, View, FlatList, SafeAreaView } from "react-native";
 import ListItem from "./components/ListItem";
-import articles from "./dummies/articles.json";
+import dummyArticles from "./dummies/articles.json";
+import Constants from "expo-constants";
+import axios from "axios";
 
-export default function App() {
+const URL = `http://newsapi.org/v2/top-headlines?country=jp&apiKey=${Constants.manifest.extra.newsApiKey}`;
+
+export default App = () => {
+  const [articles, setArticles] = useState([]);
+  useEffect(() => {
+    fetchArticles();
+  }, []);
+
+  const fetchArticles = async () => {
+    try {
+      const response = await axios.get(URL);
+      setArticles(response.data.articles);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <FlatList
@@ -19,7 +37,7 @@ export default function App() {
       />
     </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
--- components/ListItem.js
+++ components/ListItem.js
@@ -30,7 +30,12 @@ const ListItem = ({ imageUrl, author, title }) => {
   return (
     <View style={styles.itemContainer}>
       <View style={styles.leftContainer}>
-        <Image style={{ width: 100, height: 100 }} source={{ url: imageUrl }} />
+        {!!imageUrl && (
+          <Image
+            style={{ width: 100, height: 100 }}
+            source={{ url: imageUrl }}
+          />
+        )}
       </View>
       <View style={styles.rightContainer}>
         <Text numberOfLines={3} style={styles.text}>
